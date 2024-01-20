034f4ace86f0b33c86d0e8da593131bf98186c99
Atharv-Nalwade <atharv.n2002@gmail.com>
2024-01-20 16:53:43 +0530

    Added cors config

--- src/index.js
+++ src/index.js
@@ -4,16 +4,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./configs/serverConfig.js');
 const APIROUTES = require('./routes/index.js');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/api', APIROUTES);
 
+const host='0.0.0.0';
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 async function SetupAndStartServer() {
-  app.listen(PORT, () => {
+  app.listen(PORT,host, () => {
     console.log('Server is listening on port 3000');
   });
 }
